81cfb027dd335c9b92380ce501a654922640ff4d
YURA <chornei.yurii.yu@chnu.edu.ua>
2024-02-22 18:49:07 +0200

    nomer zavdanya nomer "int_within_bounds"))))

--- src/int_within_bounds.py
+++ src/int_within_bounds.py
@@ -1,3 +1,11 @@
 def int_within_bounds(number, lower_bound, upper_bound):
-  # Type your code
-  return 
+    if type(number) != int:
+      return False
+    elif number >= lower_bound and number< upper_bound:
+      return True
+    else:
+      return False
+
+
+
+print(int_within_bounds(5,5,8))
