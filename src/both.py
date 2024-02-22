fec581f32871a22c7cd7e81efc6649f88b5a32e2
YURA <chornei.yurii.yu@chnu.edu.ua>
2024-02-22 18:32:05 +0200

    nomer zavdanya nomer "both"))))

--- src/both.py
+++ src/both.py
@@ -1,3 +1,6 @@
 def both(number1, number2):
-  # Type your code
-  return
+    if number1 < 0 and number2 < 0 or number1 > 0 and number2 > 0 or number1 == 0 and number2 == 0:
+        return True
+    else:
+        return False
+print(both(6,-4))
