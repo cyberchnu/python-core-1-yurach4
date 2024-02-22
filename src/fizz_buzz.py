570e912be201a04dcb74ec24ffd369374533ccf2
YURA <chornei.yurii.yu@chnu.edu.ua>
2024-02-22 17:47:45 +0200

    nomer zavdanya nomer "addition"))))

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,3 +1,12 @@
-def fizz_buzz(param):
-  #Type your code here
-  return
+def fizz_buzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    else:
+        return str(number)
+
+
+
