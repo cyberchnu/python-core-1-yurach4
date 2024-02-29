18dfb60ec3adc82569abc29c6abdaefb164663f5
YURA <chornei.yurii.yu@chnu.edu.ua>
2024-02-29 23:08:47 +0200

    nomer zavdanya nomer "mean"))))

--- src/mean.py
+++ src/mean.py
@@ -2,7 +2,8 @@ def mean(number):
     num_str = str(number)
     digit_sum = 0
     for digit_char in num_str:
-      digit_sum += (
-        int(digit_char))
-    mean_value = digit_sum / len(num_str)
+        digit_sum += (
+            int(digit_char))
+    mean_value = (digit_sum /
+                  len(num_str))
     return mean_value
