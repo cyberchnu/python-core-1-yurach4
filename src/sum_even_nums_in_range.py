c25b5be2679bcb812a690de3045d964f9d905921
YURA <chornei.yurii.yu@chnu.edu.ua>
2024-02-29 23:14:36 +0200

    nomer zavdanya nomer "sum_even_nums_in_range"))))

--- src/sum_even_nums_in_range.py
+++ src/sum_even_nums_in_range.py
@@ -1,3 +1,6 @@
 def sum_even_nums_in_range(start, stop):
-  # Type your code
-  return
+  even_sum = 0
+  for num in range(start, stop + 1):
+    if num % 2 == 0:
+      even_sum += num
+  return even_sum
